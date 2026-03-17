70e712c7b412c53558ea6b2bf9a7a5b9597c7b09
andrescat98 <andrescat98@gmail.com>
2026-03-17 01:00:15 +0100

    first look

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: 'static', // o togli output del tutto, è il default
 });
