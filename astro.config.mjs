5725103a17bbd8fb2b1027d7abd5b3deaec55550
andrescat98 <andrescat98@gmail.com>
2026-07-02 00:39:20 +0200

    added favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://mementoconvivium.pages.dev',
   output: 'static', // o togli output del tutto, è il default
 });
